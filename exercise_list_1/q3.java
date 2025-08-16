2cdc5125098fcced9c246d2ca6e54342a37985f6
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-08-16 11:44:23 -0300

    Exercise list 1

---
+++ exercise_list_1/q3.java
@@ -0,0 +1,18 @@
+package exercise_list_1;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        float radius = sc.nextFloat();
+        System.out.print("Enter the height: ");
+        float height = sc.nextFloat();
+
+        float volume = 3.14159f * (radius * radius) * height;
+        System.out.print("Volume: " + volume);
+
+        sc.close();
+    }
+}
