55481b3f9751963e2085beedfdff9598d101b263
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-08-19 09:56:22 -0300

    Exercises java 1

--- exercise_list_1/q12.java
+++ exercise_list_1/q12.java
@@ -9,7 +9,8 @@ public class q12 {
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
         int screw = 0,nut = 0,washer = 0;
-        System.out.println("SHOP\n-----------------------\n");
+        float priceScrew = 5, priceNut = 10, priceWasher = 15;
+        System.out.println("\n-----------------------\nSHOP");
         System.out.println("-----------------------\n1 - SCREWS\n2 - NUTS\n3 - WASHERS\n0 - EXIT\n-----------------------");
         int choose;
         do
@@ -28,7 +29,6 @@ public class q12 {
                     washer++;
                     break;
                 case 0:
-                    System.exit(0);
                     break;
                 default:
                     System.out.println("Try another number");
@@ -36,6 +36,30 @@ public class q12 {
             }
         }   
         while (choose != 0); 
+
+        // Subtotals
+        float subScrew = screw * priceScrew;
+        float subNut = nut * priceNut;
+        float subWasher = washer * priceWasher;
+
+        // Discounts
+        float discScrew = subScrew * 0.2f;
+        float discNut = subNut * 0.1f;
+        float discWasher = subWasher * 0.3f;
+
+        // Totals
+        float totalDiscount = discScrew + discNut + discWasher;
+        float totalGross = subNut + subScrew + subWasher;
+        float totalToPay = totalGross - totalDiscount;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Screw: " + screw);
+        System.out.println("Nut: " + nut);
+        System.out.println("Washer" + washer);
+        System.out.printf("Full price: %.2f\n", totalGross);
+        System.out.printf("Total discount: %.2f\n", totalDiscount);
+        System.out.printf("Total to pay: %.2f", totalToPay);
 
 
         sc.close();
