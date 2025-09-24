3cf37b18d91f592edcebeaac65bb07c243a29e85
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-09-24 20:56:28 -0300

    Aula4_java_exercises

--- aula4_java/house.java
+++ aula4_java/house.java
@@ -33,4 +33,32 @@ public class House {
     {
         numDoors++;
     }
+
+    // Acessadores de variáveis privadas
+
+    public int getNumDoors() {
+        return numDoors;
+    }
+
+    public int getNumWindows() {
+        return numWindows;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    // Modificadores de variáveis privadas
+
+    public void setNumDoors(int numDoors) {
+        this.numDoors = numDoors;
+    }
+
+    public void setNumWindows(int numWindows) {
+        this.numWindows = numWindows;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 }
