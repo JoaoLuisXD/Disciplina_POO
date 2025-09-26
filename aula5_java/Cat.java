6a648bc5d4763790cfb3f0667484995154adb494
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2025-09-25 21:25:38 -0300

    Aula5_java_exercises

---
+++ aula5_java/Cat.java
@@ -0,0 +1,7 @@
+package aula5_java;
+
+public class Cat extends Animal{
+    void makeSound(){
+        System.out.println("meow");
+    }
+}
--- aula5_java/atividade1_poo.java
+++ aula5_java/atividade1_poo.java
@@ -2,7 +2,16 @@ package aula5_java;
 
 public class atividade1_poo {
     public static void main(String[] args) {
-        Animal a1 = new Dog();
-        a1.makeSound();
+        Animal animal1 = new Dog();
+        animal1.makeSound();
+
+        Animal animal2 = new Cat();
+        animal2.makeSound();
+
+        if (animal1 instanceof Dog) 
+        {
+            System.out.println("Dog");
+        }
+        else System.out.println("Cat");
     }
 }
